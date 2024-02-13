4a8650751907552700933ae910c7bc9050f39872
sisfoku <159861864+sisfoku@users.noreply.github.com>
2024-02-13 02:27:12 +0000

    Title ditambahkan contoh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Demo Aplikasi Replit dan Github</title>
+        <title>Contoh Demo Aplikasi Replit dan Github</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
